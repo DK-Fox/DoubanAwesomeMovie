f23a0f4eed4c15c6643f6e02d6ebd03e353e2bf7
DK-Fox <syc0441@163.com>
2018-11-27 20:00:39 +0800

    crawl

--- crawl/LagouInf/LagouInf/settings.py
+++ crawl/LagouInf/LagouInf/settings.py
@@ -27,13 +27,13 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
@@ -52,9 +52,10 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'LagouInf.middlewares.LagouinfDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware':None,
+    'LagouInf.middlewares.LagouinfDownloaderMiddleware': 543,
+}
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
@@ -64,9 +65,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'LagouInf.pipelines.LagouinfPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'LagouInf.pipelines.LagouinfPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
--- crawl/LagouInf/LagouInf/spiders/lagouinf.py
+++ crawl/LagouInf/LagouInf/spiders/lagouinf.py
@@ -5,7 +5,22 @@ import scrapy
 class LagouinfSpider(scrapy.Spider):
     name = 'lagouinf'
     allowed_domains = ['lagou.com']
-    start_urls = ['http://lagou.com/']
+    start_urls = ['https://www.lagou.com/']
 
     def parse(self, response):
-        pass
+        '''
+        Get login page
+        '''
+        login_page=response.css('g_tbar div.lg_tbar_l a::attr("href")').extract_first()
+        yield response.follow(login_page,self.login_parse)
+
+    def login_parse(self,reponse):
+        '''
+        Log in
+        '''
+
+    def after_login_parse(self,response):
+        '''
+        Start to get information
+        '''
+
