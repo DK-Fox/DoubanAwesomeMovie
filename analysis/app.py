98f1bfe45137a307eae87939156056a95ca7dc1e
DK-Fox <syc0441@163.com>
2018-12-04 22:46:00 +0800

    web

--- analysis/app.py
+++ analysis/app.py
@@ -1,16 +1,16 @@
 import numpy as np
 import pandas as pd
-import matplotlib
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from sqlalchemy.orm import sessionmaker
 from models import DoubanMovie, engine
+#处理中文字体的方法
+from matplotlib.font_manager import _rebuild
 
-myfont=matplotlib.font_manager.FontProperties(
-    fname='/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc'
-)
+_rebuild()
+mpl.rcParams['font.sans-serif']=u'SimHei'
+#------------------
 
-# while 1
-    # pass
 IMG_PATH="/app/img/"
 sql=r"select * from DoubanMovie;"
 df=pd.read_sql_query(sql,engine)
@@ -36,12 +36,13 @@ class FromDBDataGenerateIMG(object):
         fig1,ax1=plt.subplots()
         ax1.pie(pie1,labels=pie1.index,autopct='%1.1f%%',shadow=True,startangle=90)
         ax1.axis('equal')
-        ax1.set_title(u'测试',FontProperties=myfont)
         fig1.savefig(IMG_PATH+'pie.png')
 
     def generate_plot(self):
         plot1=df.groupby('year').size()
-        print(plot1)
+        fig2,ax2=plt.subplots()
+        ax2.plot(plot1.index,plot1)
+        fig2.savefig(IMG_PATH+'plot.png')
 
     def generate_barh(self):
         barh1=df.drop('type',axis=1).\
@@ -49,8 +50,22 @@ class FromDBDataGenerateIMG(object):
                     split('/',expand=True).\
                     stack().reset_index(level=1,drop=True).\
                     rename('type')).groupby('type').size()
+        barh1=barh1.drop('官方网站:')
+        rate=barh1/barh1.sum()<0.05
+        other=barh1[rate].sum()
+        barh1=barh1[~rate]
+        barh1['其他']=other
         print(barh1)
 
+        y_pos = np.arange(len(barh1))
+        print(y_pos)
+        print(list(barh1.index))
+        fig3,ax3=plt.subplots()
+        ax3.barh(y_pos,barh1,align="center",color='green',ecolor='black')
+        ax3.set_yticks(y_pos)
+        ax3.set_yticklabels(barh1.index)
+        ax3.invert_yaxis()
+        fig3.savefig(IMG_PATH+'barh.png')
 
 if __name__=='__main__':
     test=FromDBDataGenerateIMG()
--- website/app.py
+++ website/app.py
@@ -1,10 +1,49 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config.update(dict(
+    SQLALCHEMY_DATABASE_URI='mysql://root:123456@mymysql/douban_movie',
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    DEBUG=True
+))
+
+db= SQLAlchemy(app)
+
+class DoubanMovie(db.Model):
+    __tablename__ = 'DoubanMovie'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    url =db.Column(db.String(128))
+    year=db.Column(db.Integer)
+    type=db.Column(db.String(64))
+    location=db.Column(db.String(64))
+    summary=db.Column(db.Text)
+    score=db.Column(db.Float)
+
+    def __init__(self,name,url,year,type,location,summary,score):
+        self.name=name
+        self.url=url
+        self.year=year
+        self.type=type
+        self.location=location
+        self.summary=summary
+        self.score=score
+
 @app.route("/")
 def index():
-    return "Hello"
+    return render_template('index.html',movies=DoubanMovie.query.all())
+
+# @app.route("/<int:id>")
+# def movies(id):
+    # movie=DoubanMovie.query.get_or_404(id)
+    # return render_template('movie.html',movie=movie)
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'),404
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=80)
